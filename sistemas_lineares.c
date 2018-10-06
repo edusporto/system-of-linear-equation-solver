6166e893375bfc4fa2d0695a1a94719b108066cf
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2018-10-06 16:21:49 -0300

    continuando

--- sistemas_lineares.c
+++ sistemas_lineares.c
@@ -7,8 +7,59 @@
 #define ENTRE(valor, min, max) (min <= valor && valor <= max)
 
 
+
 /* =========================================================== */
-/*                     FUNCOES AUXILIARES                      */
+/*                            LISTA                            */
+/* =========================================================== */
+/* Sera transferida para uma biblioteca posteriormente */
+
+typedef
+    struct No {
+        void* info;
+        size_t tamanho;
+        struct No* prox;
+    } No;
+
+void inicializa_no(No* n, void* info, size_t tamanho, No* prox) {
+    n->info    = info;
+    n->tamanho = tamanho;
+    n->prox    = prox;
+}
+
+typedef
+    struct {
+        No* prim;
+        No* ult;
+    } Lista;
+
+Lista new_lista() {
+    Lista ret;
+    ret.prim = NULL;
+    ret.ult   = NULL;
+    return ret;
+}
+
+void inserir_final_lista(Lista* lis, void* valor, size_t tamanho_valor) {
+    /* alocacao de memoria do que sera inserido na lista */
+    void* copia = malloc(tamanho_valor);
+    memcpy(copia, valor, tamanho_valor);
+    No* novo = (No*)malloc(sizeof(No));
+
+    inicializa_no(novo, copia, tamanho_valor, NULL);
+
+    if (lis->prim == NULL) {
+        lis->prim = novo;
+        lis->ult  = novo;
+        return;
+    }
+
+    lis->ult->prox = novo;
+    lis->ult = novo;
+}
+
+
+/* =========================================================== */
+/*                 FUNCOES E STRUCTS AUXILIARES                */
 /* =========================================================== */
 
 char terminar() {
@@ -77,7 +128,7 @@ int main(void) {
 
     } while (terminar() != 'S');
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
