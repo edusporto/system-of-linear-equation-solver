26e8586a7bae71cf7bd0dbd49cec70cee4d28a87
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2018-10-11 14:39:02 -0300

    Movendo lista para arquivo header

--- lista.h
+++ lista.h
@@ -1,5 +1,5 @@
-#ifndef _LISTA_H_
-#define _LISTA_H_
+#ifndef LISTA_H
+#define LISTA_H
 
     typedef
         struct No {
@@ -13,5 +13,21 @@
             No* prim;
             No* ult;
         } Lista;
+
+    void no_inicializa(No* n, void* info, size_t tamanho, No* prox);
+
+    Lista lista_new();
+
+    void lista_inserir_fim(Lista* lis, void* valor, size_t tamanho_valor);
+
+    void lista_inserir_inicio(Lista* lis, void* valor, size_t tamanho_valor);
+
+    void* lista_get_item(Lista* lis, int index);
+
+    char lista_remover_primeiro(Lista* lis);
+
+    char lista_remover_ultimo(Lista* lis);
+
+    char lista_remover_item(Lista* lis, int index);
 
 #endif
